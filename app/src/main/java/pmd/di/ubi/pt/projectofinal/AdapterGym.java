2a4942ca318589d1a19ccd22459e92b2129a8e02
delciokelson <delciokelson@outlook.com>
2020-05-31 02:13:51 +0100

    map of gyms add

---
+++ app/src/main/java/pmd/di/ubi/pt/projectofinal/AdapterGym.java
@@ -0,0 +1,82 @@
+package pmd.di.ubi.pt.projectofinal;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AdapterGym extends RecyclerView.Adapter<AdapterGym.ComentarioHolder> {
+
+    private Context context;
+    private ArrayList<Map<String, Object>> comentariosList;
+
+    public AdapterGym(Context context, ArrayList<Map<String, Object>> comentariosList) {
+        this.context = context;
+        this.comentariosList = comentariosList;
+    }
+
+    @NonNull
+    @Override
+    public ComentarioHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.card_comentario,parent,false);
+        return new ComentarioHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ComentarioHolder holder, int position) {
+        Map<String, Object> comentario = comentariosList.get(position);
+        holder.setDetails(comentario);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return comentariosList!=null?comentariosList.size():0;
+    }
+
+
+    public class ComentarioHolder extends RecyclerView.ViewHolder {
+        public ComentarioHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void setDetails(Map<String, Object> comentario) {
+            final TextView txtComentador = itemView.findViewById(R.id.nome_comentador);
+            final TextView txtComentario = itemView.findViewById(R.id.comentario);
+            final RatingBar ratingBar = itemView.findViewById(R.id.comentario_rating);
+            final ImageView imgPerfil = itemView.findViewById(R.id.img_comentador);
+
+            txtComentador.setText((String) comentario.get("nomeComentador"));
+            txtComentario.setText((String)comentario.get("comentario"));
+
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference("image/"+comentario.get("uidUsuario"));
+
+            final long ONE_MEGABYTE = 1024 * 1024;
+            storageReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(bytes -> {
+                if(bytes.length!=0){
+                    try {
+                        Glide.with(context.getApplicationContext())
+                                .load(bytes)
+                                .into(imgPerfil);
+                    }catch (Exception ignored){
+
+                    }
+                }
+            });
+            ratingBar.setRating(Float.parseFloat((String) comentario.get("ratingComentario")));
+        }
+    }
+}
---
+++ app/src/main/java/pmd/di/ubi/pt/projectofinal/FragmentMapGyms.java
@@ -0,0 +1,56 @@
+package pmd.di.ubi.pt.projectofinal;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.tabs.TabLayout;
+
+
+public class FragmentGyms extends Fragment  implements OnMapReadyCallback {
+    // Include the OnCreate() method here too, as described above.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public FragmentGyms() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gyms, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync( this);
+
+        return view;
+    }
+
+
+}
+
+
